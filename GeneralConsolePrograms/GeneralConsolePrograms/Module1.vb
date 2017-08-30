d0d501b875ff0eced7ef7242855e0d34211aef22
Mann2019 <manpreetbawa04@gmail.com>
2017-08-30 21:58:18 +0530

    Console Apps folder created

---
+++ GeneralConsolePrograms/GeneralConsolePrograms/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num As Integer
+        num = Convert.ToInt32(Console.ReadLine)
+        Console.WriteLine(num)
+        Console.ReadLine()
+    End Sub
+
+End Module
